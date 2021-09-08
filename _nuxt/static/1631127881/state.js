9211877430f24a6bff6997ba4d18df4f576b7e95
strvworks <strvworks@users.noreply.github.com>
2021-09-08 19:08:37 +0000

    deploy: 801601d94e935a49b0772dc54accc3f2deebd899

--- _nuxt/static/1631127881/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631127881",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"53336f38"}},globalRefs:{}}}(null,"\u002F"));
